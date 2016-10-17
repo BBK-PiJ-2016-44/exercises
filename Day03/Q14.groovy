057ac26f458da3fc9d1465452b4b4858414ec83d
Keith Mannock <keith@dcs.bbk.ac.uk>
2016-10-17 20:30:51 +0100

    Q14

--- Day03/Q14.groovy
+++ Day03/Q14.groovy
@@ -1,21 +1,32 @@
-def cons = new BufferedReader(new InputStreamReader(System.in))
-
 //get input text and its length
 
-print "Please write a text: "
-String inputText = cons.readLine()
+print "Please input some text: "
+String inputText = System.console().readLine()
 int inputLength = inputText.length()
 
 //print forwards
 print inputText
 
-//print backwards
+//print backwards using a while loop
 
 int i = 0
 while (i < inputLength) {
     backLetter = inputText.charAt(inputLength - i - 1)
     print backLetter
     i++
+}
+
+println()
+
+//print forwards
+
+print inputText
+
+//print backwards using a for loop
+
+for(i=0; i < inputLength; i++){
+    backLetter = inputText.charAt(inputLength - i - 1)
+    print backLetter    
 }
 
 println()
